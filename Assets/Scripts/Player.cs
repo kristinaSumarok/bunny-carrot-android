ff9b4f80a9b65045e6f830f5e0ce965fb207e83a
kristinaSumarok <ktroitski33@gmail.com>
2024-11-11 16:07:10 +0200

    destroy carrot on collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour
     public FixedJoystick joystick;
     public float moveSpeed;
     float hInput, vInput;
+    int score = 0;
+    public GameObject winText;
+    public int winScore;
     void Start()
     {
         
@@ -20,5 +23,18 @@ public class Player : MonoBehaviour
         vInput = joystick.Vertical * moveSpeed;
 
         transform.Translate(hInput,vInput,0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Carrot")
+        {
+            score++;
+            Destroy(collision.gameObject);
+            if (score >= winScore)
+            {
+                winText.SetActive(true);
+            }
+        }
     }
 }
